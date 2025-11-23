2b8183dc5fd3aab2882e3d94a09049f799d7c7da
wsuits6 <wsuits6@gmail.com>
2025-11-23 00:04:02 +0000

    just testing some operator on javascript

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,1 +1,20 @@
-//JS for the JS
+let A = 1;
+let B = 2;
+
+// Addition
+console.log("Addition:", A + B);
+
+// Subtraction
+console.log("Subtraction:", A - B);
+
+// Multiplication
+console.log("Multiplication:", A * B);
+
+// Modulus
+console.log("Modulus:", B % A);
+
+// Increment
+console.log("Increment");
+console.log("First value of A:", A);
+A++;
+console.log("Value of A after increment:", A);
