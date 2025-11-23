d538f5c344d354d8e144f629506243aa2db73c03
wsuits6 <wsuits6@gmail.com>
2025-11-23 00:07:14 +0000

    added decremnt

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,20 +1,27 @@
 let A = 1;
-let B = 2;
+let B = 2
 
 // Addition
-console.log("Addition:", A + B);
+console.log("Addition:", A + B)
 
 // Subtraction
-console.log("Subtraction:", A - B);
+console.log("Subtraction:", A - B)
 
 // Multiplication
-console.log("Multiplication:", A * B);
+console.log("Multiplication:", A * B)
 
 // Modulus
-console.log("Modulus:", B % A);
+console.log("Modulus:", B % A)
 
 // Increment
 console.log("Increment");
-console.log("First value of A:", A);
+console.log("First value of A:", A)
 A++;
-console.log("Value of A after increment:", A);
+console.log("Value of A after increment:", A)
+
+
+// Decrement
+console.log("Decrement");
+console.log("First Value of B:", B)
+B--;
+console.log("value of B after drement",B)
