36334fb800d73c604d0224e88dc7a2a2270a7c14
wsuits6 <wsuits6@gmail.com>
2025-11-24 11:38:05 +0000

    just styling with border to prepare for responsiveness

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -25,3 +25,47 @@ console.log("Decrement");
 console.log("First Value of B:", B)
 B--;
 console.log("value of B after drement",B)
+
+console.log("=======================================")
+console.log("Comparison Operator")
+console.log("New Values of A and B")
+A = 1
+B = 2
+
+console.log("A is Equal to:", A)
+console.log("B is Equal to:", B)
+
+console.log("====================")
+
+//Equal operator
+console.log(A == B)
+console.log("A is not eqaul (==)  to B")
+
+//Not Equal To Operator 
+console.log(B != A)
+console.log(" B is NOT equal to A")
+
+
+//less than Oprator 
+console.log(A < B)
+console.log("A is less  than B")
+
+console.log(B < A)
+console.log("B is not equalt to A")
+
+
+//Greater than Operator 
+console.log(B > A)
+console.log("B is Greater than A")
+console.log(A > B)
+console.log("A is not greater than B")
+
+
+//Greater than or equal to operator 
+console.log(B >= A)
+console.log(A >= B)
+
+//Less than or equal to 
+console.log(B <= A)
+console.log(A <= B)
+
